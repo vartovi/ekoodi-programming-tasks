8c511d63a4f7993b1f27e7ac6c2df26e3de8131b
vartovi <viljo.varto@gmail.com>
2017-02-15 12:43:35 +0200

    Add project files.

---
+++ bban-to-iban/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bban_to_iban
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
