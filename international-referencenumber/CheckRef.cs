10571a252d1c577d0a27330439a25d47b497ed73
vartovi <viljo.varto@gmail.com>
2017-03-01 12:13:42 +0200

    First working version

---
+++ international-referencenumber/CheckRef.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace international_referencenumber
+{
+    class CheckRef
+    {
+        public static string CheckRefNum(string refNumber)
+        {
+            if (!refNumber.Contains("RF"))
+            {
+                return "Invalid referencenumber. Please add RF";
+            }
+            refNumber = LettersToNumbers(refNumber);
+            refNumber = Regex.Replace(refNumber, @"[^\d]", "");
+            var toDecimal = Convert.ToDecimal(refNumber) % 97;
+
+            if (toDecimal == 1)
+            {
+                return "Your referencenumber is correct\nChecksum = " + toDecimal;
+            }
+
+            return "Your referencenumber is incorrect\nCheckum " + toDecimal + " needs to be 1";
+
+        }
+
+        public static string LettersToNumbers(string refNumber)
+        {
+            char[] letters = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z' };
+
+            refNumber = refNumber.Substring(4) + refNumber.Substring(0, 4);
+
+            for (int i = 0; i < 25; i++)
+            {
+                int replacement = 10 + i;
+                refNumber = Regex.Replace(refNumber, Convert.ToString(letters[i]), Convert.ToString(replacement));
+            }
+
+            return refNumber;
+        }
+    }
+}
--- international-referencenumber/Program.cs
+++ international-referencenumber/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,29 @@ namespace international_referencenumber
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Select: \n1:Check international referencenumber \n2:Create international referencenumber");
+            int userInput = Convert.ToInt16(Console.ReadLine());
+            string message;
+
+            switch (userInput)
+            {
+                case 1:
+                    Console.Write("Write international referencenumber to check: ");
+                    var refToCheck = Console.ReadLine();
+                    message = CheckRef.CheckRefNum(refToCheck);
+                    Console.WriteLine(message);
+                    break;
+                case 2:
+                    Console.Write("Write regular referencenumber: ");
+                    var refToCreate = Console.ReadLine();
+                    message = CreateRef.CreateRefNum(refToCreate);
+                    Console.WriteLine(message);
+                    break;
+                default:
+                    Console.WriteLine("Invalid selection");
+                    break;
+            }
+            Console.ReadKey();
         }
     }
 }
