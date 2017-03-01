e4fc5f47446a5453effaa41b83a4718eeb0fefc9
vartovi <viljo.varto@gmail.com>
2017-03-01 12:42:01 +0200

    Add project files.

---
+++ ski-jumping-points-calculator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ski_jumping_points_calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
